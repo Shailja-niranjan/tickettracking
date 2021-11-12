61582ca2fc46a1f86988b707362b195fe16b6a0a
shailja-niranjan <shailja.niranjan@gmail.com>
2021-11-12 10:13:19 +0530

    right top icons added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,29 +2,47 @@ import React from 'react'
 import Typography from '@material-ui/core/Typography'
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import SearchBar from "material-ui-search-bar";
-import Box from '@material-ui/core/Box';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core';
+import HelpIcon from '@material-ui/icons/Help';
+import SettingsIcon from '@material-ui/icons/Settings';
+import PermIdentitySharpIcon from '@material-ui/icons/PermIdentitySharp';
+// import Box from '@material-ui/core/Box';
 
 // import Header from "./pages/Header"
-// import Header from "./pages/Header"
+
+const useStyles = makeStyles({
+   heading: {
+     
+   }
+
+
+})
+
+
+
+
 
 export default function Homepage() {
+  const classes = useStyles()
   return (
-    
-    <div>
-     <box width={100} ><Typography
-      color="primary"
-      component="h2"
-      padding-left="10px"
-    >
-      Troubleshooting 
-      <ArrowDropDownIcon color="primary" /></Typography></box>
-    
- <SearchBar 
-style={{
-  margin: "0 auto",
-  maxWidth: 400
-}}/>
-</div>
-     
+    <Container size="lg">
+      <Typography
+        variant="h6" 
+        color="primary"
+        component="h2"
+        gutterBottom
+      >
+        Troubleshooting 
+        <ArrowDropDownIcon color="primary" /></Typography>
+      <SearchBar placeholder="search platform for tool.."style={{
+      margin: "0 auto",
+      maxWidth: 400
+    }}/> 
+    <HelpIcon color="primary"/> 
+    <SettingsIcon color="primary"/>
+    <PermIdentitySharpIcon >add_circle</PermIdentitySharpIcon>
+     </Container> 
+   
   )
 }
