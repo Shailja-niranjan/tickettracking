5c2a1d1467ffb08060b468d12f37eb5233b4c2a0
shailja-niranjan <shailja.niranjan@gmail.com>
2021-11-12 16:58:39 +0530

    header component allignment

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography'
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import SearchBar from "material-ui-search-bar";
+import SearchBar from 'material-ui-search-bar';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core';
 import HelpIcon from '@material-ui/icons/Help';
@@ -10,39 +10,35 @@ import PermIdentitySharpIcon from '@material-ui/icons/PermIdentitySharp';
 // import Box from '@material-ui/core/Box';
 
 // import Header from "./pages/Header"
-
-const useStyles = makeStyles({
-   heading: {
-     
-   }
-
-
-})
-
-
-
-
+import Grid from '@mui/material/Grid';
 
 export default function Homepage() {
-  const classes = useStyles()
   return (
     <Container size="lg">
-      <Typography
-        variant="h6" 
-        color="primary"
-        component="h2"
-        gutterBottom
-      >
-        Troubleshooting 
-        <ArrowDropDownIcon color="primary" /></Typography>
-      <SearchBar placeholder="search platform for tool.."style={{
-      margin: "0 auto",
-      maxWidth: 400
-    }}/> 
-    <HelpIcon color="primary"/> 
-    <SettingsIcon color="primary"/>
-    <PermIdentitySharpIcon >add_circle</PermIdentitySharpIcon>
-     </Container> 
-   
-  )
+      <Grid container spacing={2}>
+        <Grid item xs={3}>
+          <Typography variant="h6" color="primary" component="h2" gutterBottom>
+            Troubleshooting
+            <ArrowDropDownIcon color="primary" />
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <SearchBar
+            placeholder="search platform for tool.."
+            style={{
+              margin: '0 auto',
+              maxWidth: 400,
+            }}
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <div style={{ textAlign: 'right' }}>
+            <HelpIcon color="primary" />
+            <SettingsIcon color="primary" />
+            <PermIdentitySharpIcon>add_circle</PermIdentitySharpIcon>
+          </div>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
